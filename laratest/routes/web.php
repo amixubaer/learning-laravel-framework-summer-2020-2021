4ffdf73e546d1977420d159d7e4550cd48896489
Abu Jubaer <18-38784-3@student.aiub.edu>
2021-06-12 17:32:22 +0600

    crud

--- laratest/routes/web.php
+++ laratest/routes/web.php
@@ -14,3 +14,26 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', ['uses'=>'LoginController@index']);
+Route::post('/login', 'LoginController@verify');
+Route::get('/home', 'HomeController@index');
+Route::get('/logout', 'LogoutController@index');
+Route::get('/user/list', 'UserController@index');
+
+Route::get('/user/create', 'UserController@create');
+Route::post('/user/create', 'UserController@insert');
+Route::get('/user/details/{id}', 'UserController@details');
+
+Route::get('/user/edit/{id}', 'UserController@edit');
+Route::post('/user/edit/{id}', 'UserController@update');
+
+Route::get('/user/delete/{id}', 'UserController@delete');
+Route::post('/user/delete/{id}', 'UserController@destroy');
+
+
+
+
+Route::get('/register', function(){
+	echo "this is signup page...";
+});
